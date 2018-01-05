3c7fe6000c641b743714d52b5123327b74c3ff77
Jay <stoltzenberg@gmail.com>
2018-01-05 17:50:22 +0100

    Decision Tree Commit

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,7 +19,15 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.metrics import accuracy_score
+from sklearn import tree
 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+acc = accuracy_score(labels_test, pred)
+print acc
 
 
 #########################################################
